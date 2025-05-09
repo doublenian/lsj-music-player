46a8d102bd324c9d706205c72caeb22ed331d0cc
解念念 <doublenian.xie@gmail.com>
2025-05-09 16:19:34 +0800

    一个测试bolt的github功能

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { MusicPlayer } from "@/components/music-player";
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <h1>我是测试</h1>
       <MusicPlayer />
     </div>
   );

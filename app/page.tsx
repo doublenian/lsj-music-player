6d4d74a0c1876823ff75c6666651cd29eff2f1f9
解念念 <doublenian.xie@gmail.com>
2025-05-09 16:20:37 +0800

    去掉测试

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { MusicPlayer } from "@/components/music-player";
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>我是测试</h1>
       <MusicPlayer />
     </div>
   );

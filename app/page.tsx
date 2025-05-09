69066a5dc0d1c638298efca4afb00c0958d38cb6
解念念 <xieniannian@hworld.com>
2025-05-09 17:01:39 +0800

    测试

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { MusicPlayer } from "@/components/music-player";
 
-export default function Home() {
+export default function Home(): React.ReactNode {
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <h1>Hello World</h1>
       <MusicPlayer />
     </div>
   );
